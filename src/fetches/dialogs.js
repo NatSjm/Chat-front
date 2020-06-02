c02493b2ff7fef5ec9788c586dae2177e52b64aa
ihor <ihor.bielchenko@drivedatum.com>
2020-06-02 16:47:20 +0300

    update

--- src/fetches/dialogs.js
+++ src/fetches/dialogs.js
@@ -1,14 +1,20 @@
 import axios from 'axios';
 
+const refreshTokens = () => {};
 const dialogs = async (action = () => {}) => {
-	const response = await axios.get(`http://127.0.0.1:4444/dialogs`);
+	try {
+		const response = await axios.get(`http://127.0.0.1:4444/dialogs`);
 
-	action((state) => {
-		return {
-			...state,
-			data: response.data,
-		};
-	});
+		action((state) => {
+			return {
+				...state,
+				data: response.data,
+			};
+		});
+	}
+	catch (err) {
+		err.response.status === 401 && refreshTokens(err);
+	}
 };
 
 export default dialogs;
